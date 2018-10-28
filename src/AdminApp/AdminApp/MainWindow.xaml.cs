92f805caa85690bad2c5928a5c8cf9dda8bba788
littleAnarchy <korabelwork@gmail.com>
2018-10-28 18:49:43 +0200

    Threading timer studied

---
+++ src/AdminApp/AdminApp/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AdminApp.ViewModels;
+using System.Windows;
+
+namespace AdminApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainGrid.DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ src/AdminApp/AdminApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace AdminApp.ViewModels
+{
+    public class MainViewModel : BasicViewModel
+    {
+        private readonly Timer _timer;
+        private readonly Random _random = new Random();
+        private double _num = 5;
+
+        public double Num
+        {
+            get => _num;
+            set
+            {
+                if (_num == value) return;
+                _num = value;
+                DoPropertyChanged();
+            }
+        }
+           
+
+        public MainViewModel()
+        {
+            _timer = new Timer(o => { Num = _random.NextDouble(); }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1) );
+        }
+    }
+}
